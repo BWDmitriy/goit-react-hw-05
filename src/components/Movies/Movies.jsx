ddf63e617fcf6601fa223e51c92a3e2afc5bdfe9
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-05-15 22:21:59 +0300

    Movies styles

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,7 @@
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import styles from "./Movies.module.css";
 
 export default function Movies() {
   let { id } = useParams();
@@ -27,27 +28,27 @@ export default function Movies() {
 
   // Render movie details or loading state
 return (
-  <div className="movie-container">
-    <Link to="/">
-        <button>&larr; Go back</button>
+    <div className={styles['movie-container']}>
+      <Link to="/">
+        <button className={styles['goBack-button']}>&larr; Go back</button>
       </Link>
-    {movieDetails && (
-      <>
-        <img src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`} alt={movieDetails.title} />
-        <div className="movie-info">
-          <h1>{movieDetails.title} ({new Date(movieDetails.release_date).getFullYear()})</h1>
-          <p>User score: {Math.round(movieDetails.vote_average * 10)}%</p>
-          <h2>Overview</h2>
-          <p>{movieDetails.overview}</p>
-          <h2>Genres</h2>
-          <ul>
-            {movieDetails.genres.map(genre => (
-              <li key={genre.id}>{genre.name}</li>
-            ))}
-          </ul>
+      {movieDetails && (
+        <div className={styles['movie-content']}>
+          <img className={styles['movie-poster']} src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`} alt={movieDetails.title} />
+          <div className={styles['movie-info']}>
+            <h1>{movieDetails.title} ({new Date(movieDetails.release_date).getFullYear()})</h1>
+            <p>User score: {Math.round(movieDetails.vote_average * 10)}%</p>
+            <h2>Overview</h2>
+            <p>{movieDetails.overview}</p>
+            <h2>Genres</h2>
+            <ul>
+              {movieDetails.genres.map(genre => (
+                <li key={genre.id}>{genre.name}</li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </>
-    )}
-  </div>
-);
+      )}
+    </div>
+  );
 }
