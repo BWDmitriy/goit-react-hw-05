07c595575361a2760a866dacc8631e4d025b059a
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-05-15 13:31:53 +0300

    init

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,78 +1,37 @@
-import { useEffect, useState, useCallback } from 'react';
 import './App.css';
-import ImageGallery from '../ImageGallery/ImageGallery';
 import axios from 'axios';
-import SearchBar from '../SearchBar/SearchBar';
-import Loader from '../Loader/Loader';
-import ErrorMessage from '../ErrorMessage/ErrorMessage';
-import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
-import ImageModal from '../ImageModal/ImageModal';
+import { Routes, Route } from "react-router-dom";
+
+
+
+
+
+
+
 
 function App() {
-  const [images, setImages] = useState([]);
-  const [query, setQuery] = useState("");
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const fetchImages = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await axios.get(
-        `https://api.unsplash.com/search/photos?client_id=WWP0ZkMHW4a1CGgA-GBI0FrCCywjnB3L6d04IFuYIlk&query=${query}&page=${page}`
-      );
-      const newImages = response.data.results;
-      if (page === 1) {
-        setImages(newImages);
-      } else {
-        setImages(prevImages => [...prevImages,...newImages]);
-      }
-    } catch (error) {
-      setError('Failed to fetch images');
-    } finally {
-      setIsLoading(false);
-    }
-  }, [query, page]);
+  const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
 
-  useEffect(() => {
-    if (query) {
-    fetchImages();
-  }
-  }, [fetchImages]);
+const options = {
+headers: {
 
-  const loadMore = () => {
-    setPage(prevPage => prevPage + 1);
-  };
+ Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTcwOTQxOGQ2NTZhMDNhMWI0ZWQwNzdlMzkyZDA0OCIsInN1YiI6IjY2NDQ4MDlkZjI0NDJkZWJhNjI0MWFjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vJ1U0cFA7kXEaVVVwmgagpjanAVx2duiX60FjQGMlzQ'
+ }
+};
 
-  const handleSearchSubmit = (inputValue) => {
-    setQuery(inputValue);
-    setPage(1);
-    setImages([]);
-  };
-const handleImageClick = (image) => {
-    setSelectedImage(image);
-    setIsModalOpen(true);
-  };
-   const closeModal = () => {
-    setIsModalOpen(false);
-  };
+axios.get(url, options)
+ .then(response => console.log(response))
+ .catch(err => console.error(err));
+
+
   return (
     <div>
-      <SearchBar onSubmit={handleSearchSubmit} />
-      {images.length > 0 && <ImageGallery images={images} onImageClick={handleImageClick} />}
-       {isModalOpen && selectedImage && (
-        <ImageModal
-          isOpen={isModalOpen}
-          selectedImage={selectedImage}
-          onRequestClose={closeModal}
-        />
-      )}
-      {isLoading && <Loader />}
-      {error && <ErrorMessage message={error} />}
-      {images.length > 0 && <LoadMoreBtn loadMore={loadMore} />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
     </div>
   );
 }
