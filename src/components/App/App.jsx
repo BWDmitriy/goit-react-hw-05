34115c8498c50e3fa42d15c1b7d8682d5069f5ac
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-05-17 12:49:22 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,8 @@ import Navbar from '../Navbar/Navbar';
 // import Home from '../Home/Home';
 // import Movies from '../Movies/Movies';
 // import SearchMovies from '../SearchMovies/SearchMovies';
-import { lazy, React, Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
+
 const Home = React.lazy(() => import('../Home/Home'));
 const Movies = React.lazy(() => import('../Movies/Movies'));
 const SearchMovies = React.lazy(() => import('../SearchMovies/SearchMovies'));
@@ -14,10 +15,10 @@ function App() {
     <>
       <Navbar />
       <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies/:id" element={<Movies />} /> 
-        <Route path="/movies" element={<SearchMovies />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies/:id" element={<Movies />} /> 
+          <Route path="/movies" element={<SearchMovies />} />
         </Routes>
       </Suspense>
     </>
